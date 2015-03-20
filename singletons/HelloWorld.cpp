74e0fc952f2faf4cd30a14fe76cdfe6a30afc3ee
jesse9 <zhouxing05@gmail.com>
2015-03-19 22:46:42 -0400

    Singleton Makefile

---
+++ singletons/HelloWorld.cpp
@@ -0,0 +1,18 @@
+/*
+ * HelloWorld.cpp
+ *
+ *  Created on: Mar 19, 2015 10:36:26 PM 
+ *      Author: Xing
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	cout << "Hello, World!" << endl;
+	return 0;
+}
+
+
+
